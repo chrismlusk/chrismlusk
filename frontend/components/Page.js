588615f16dd0d7d2b90c886f55b96e122700e6c6
Chris Lusk <chris.m.lusk@gmail.com>
2018-11-13 16:52:58 -0600

    Page and exports

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -2,18 +2,22 @@ import React, { Component, Fragment } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import Meta from './Meta';
 import Header from './Header';
-import { theme, ResetStyles, GlobalStyles, ContainerStyles } from './styles';
+import Main from './Main';
+import Footer from './Footer';
+import { theme, GlobalStyles, ContainerStyles } from './styles';
 
 class Page extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
         <Fragment>
-          <ResetStyles />
           <GlobalStyles />
           <Meta />
           <Header />
-          <main>{this.props.children}</main>
+          <Main>
+            {this.props.children}
+          </Main>
+          <Footer />
         </Fragment>
       </ThemeProvider>
     );
--- frontend/components/index.js
+++ frontend/components/index.js
@@ -1,5 +1,8 @@
-export { default as Header } from './Header';
 export { default as Logo } from './Logo';
 export { default as Meta } from './Meta';
 export { default as Page } from './Page';
+
 export { default as Welcome } from './Welcome';
+export { default as AboutMe } from './AboutMe';
+export { default as Projects } from './Projects';
+export { default as Contact } from './Contact';
