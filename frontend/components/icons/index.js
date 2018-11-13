3950e11a8d0494c373a436e05c3c21b64003b1b2
Chris Lusk <chris.m.lusk@gmail.com>
2018-11-13 16:52:18 -0600

    make svg icons into components

---
+++ frontend/components/icons/index.js
@@ -0,0 +1,5 @@
+export { default as GitHub } from './GitHub';
+export { default as LinkedIn } from './LinkedIn';
+export { default as Instagram } from './Instagram';
+export { default as Twitter } from './Twitter';
+export { default as External } from './External';
