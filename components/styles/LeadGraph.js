50efe4b8f1972358b68c400590fa69a9801c9fd0
Chris Lusk <chris.m.lusk@gmail.com>
2018-11-13 19:11:51 -0600

    decrease lead graph size

--- components/styles/LeadGraph.js
+++ components/styles/LeadGraph.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const LeadGraph = styled.p`
-  font-size: 1.5em;
+  font-size: 1.25em;
 
   @media (min-width: ${({ theme }) => theme.breakpoint.md}) {
     font-size: 1.375em;
