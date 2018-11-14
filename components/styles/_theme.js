0f48a08c512c2b9d9d6d4bc9147541a420805617
Chris Lusk <chris.m.lusk@gmail.com>
2018-11-14 11:27:59 -0600

    rm unused theme vars

--- components/styles/_theme.js
+++ components/styles/_theme.js
@@ -17,7 +17,6 @@ _.color = {
   black: `#000`,
 
   red: `#b02f1d`,
-  redLight: `#fc432a`,
   blue: `#2f1db0`, // or try 1d75fc
   green: `#1db02f` // or try 1db06a
 };
@@ -30,11 +29,6 @@ _.secondary =               _.color.gray600;
 _.success =                 _.color.green;
 _.light =                   _.color.gray100;
 _.dark =                    _.color.gray800;
-
-
-// Spacing
-
-_.spacer =                  `1rem`;
 
 
 // Body
@@ -55,14 +49,8 @@ _.breakpoint = {
   xl:  `1200px`
 };
 
-// Components
 
-_.border = {
-  base: `1px solid ${_.color.gray300}`,
-  width: `1px`,
-  color: _.color.gray300,
-  radius: `.25rem`
-};
+// Components
 
 _.transition = {
   speed: `116ms`,
@@ -78,25 +66,6 @@ _.fontFamily = {
   serif: `Georgia, serif`,
   mono: `'SF Mono', 'Fira Mono', 'Roboto Mono', Monaco, monospace;`
 };
-
-_.fontSize = {
-  base: `1rem`,
-  small: `80%`,
-  h1: `2.5rem`,
-  h2: `2rem`,
-  h3: `1.75rem`,
-  h4: `1.5rem`,
-  h5: `1.25rem`,
-  h6: `1rem`
-};
-
-_.lineHeight = {
-  tightest: 1.1,
-  tight: 1.3333,
-  normal: 1.5,
-  loose: 1.6666
-};
-
 
 
 export default _;
